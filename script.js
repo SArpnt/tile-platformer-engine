bdbeab7d54f05816106753cb014de3076afbb1a9
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-02-12 11:52:47 -0500

    starting render engine
    on phone so code is a mess

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ const ctx = canvas.getContext('2d')
 const level = [
     [0,0,0],
     [0,0,0],
-    [0,0,0]
+    [1,1,1]
 ]
 
-const tiles = [
-    {
-        img: document.createElement()
-    }
+const tiles = 
+{img:imgCreate('test0.png')
+    },{img:imgCreate('test1.png')}
 ]
+
+function imgCreate(src) {
+return document.createElement('img').setAttribute('src',src)
+}
 
 function step(delta) {
-    console.log(level[0][0])
-    
+    draw(0,0)
     requestAnimationFrame(step)
 }
-function draw(x,y,) { //fix errors
-    ctx.drawImage(level[0][0],x,y)
+function draw(ox,oy) {
+for(let y in level) {
+for(let x in level[y]){
+    ctx.drawImage(tiles[level[y][x]].img,x*16+ox,y*16+oy)
+}}
 }
 requestAnimationFrame(step)
