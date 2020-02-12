2935ef59b4a3c9a712b4346fc77ff3d025c4226a
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-02-12 16:47:14 -0500

    tile rendering
    tile images and level are temporary

--- script.js
+++ script.js
@@ -2,28 +2,52 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 const level = [
-    [0,0,0],
-    [0,0,0],
-    [1,1,1]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0],
+	[0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0],
+	[1, 1, 1, 1, 2, 2, 2, 2, 2, 6, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-const tiles = 
-{img:imgCreate('test0.png')
-    },{img:imgCreate('test1.png')}
+const tiles = [
+	{ name: 'empty' },
+	{ name: 'dirt' },
+	{ name: 'floor' },
+	{ name: 'ceiling' },
+	{ name: 'Rwall' },
+	{ name: 'Lwall' },
+	{ name: 'RUcorner' },
+	{ name: 'LUcorner' },
+	{ name: 'RDcorner' },
+	{ name: 'LDcorner' },
 ]
-
-function imgCreate(src) {
-return document.createElement('img').setAttribute('src',src)
+for (x in tiles) {
+	let i = document.createElement('img')
+	i.setAttribute("src", "assets/tile/" + tiles[x].name + '.png')
+	tiles[x].img = i
 }
 
 function step(delta) {
-    draw(0,0)
-    requestAnimationFrame(step)
+	ctx.fillStyle = "magenta"
+	ctx.fillRect(0, 0, 640, 480)
+	draw(0, 0)
+	requestAnimationFrame(step)
 }
-function draw(ox,oy) {
-for(let y in level) {
-for(let x in level[y]){
-    ctx.drawImage(tiles[level[y][x]].img,x*16+ox,y*16+oy)
-}}
+function draw(ox, oy) {
+	for (let y in level) {
+		for (let x in level[y]) {
+			ctx.drawImage(tiles[level[y][x]].img, (x * 16 + ox) * 2, (y * 16 + oy) * 2)
+		}
+	}
 }
 requestAnimationFrame(step)
