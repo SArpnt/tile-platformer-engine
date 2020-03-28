dbcdf580dd20fe8fd5e8ce0fc76c7a0c347277d9
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-03-28 13:37:42 -0400

    jquery and better fps and tps counters

--- script.js
+++ script.js
@@ -5,35 +5,37 @@ var cSprites = [
 	new sprite.player(64, 160),
 	new sprite.enemy(96, 160)
 ]
+{
+	let tpsC = performance.now()
+	let fpsC = performance.now()
+	function step() {
+		runSprites()
 
-function step() {
-	runSprites()
+		scrollX = Math.round(Math.max(Math.min(scrollX, 0), (level[0].length * -16) + 320)) //keep scrolling in boundaries and round
+		scrollY = Math.round(Math.max(Math.min(scrollY, 0), (level.length * -16) + 240))
 
-	scrollX = Math.round(Math.max(Math.min(scrollX, 0), (level[0].length * -16) + 320)) //keep scrolling in boundaries and round
-	scrollY = Math.round(Math.max(Math.min(scrollY, 0), (level.length * -16) + 240))
+		window.setTimeout(step, 8) //125 tps
+		$("#tps")[0].innerHTML = Math.round(1000 / (performance.now() - tpsC))
+		tpsC = performance.now()
+	}
+	function draw(ms) {
+		ctx.fillStyle = "magenta"
+		ctx.fillRect(0, 0, 640, 480)//temporary sky
 
-	window.setTimeout(step, 8.333333333333334) //120 tps
-	document.getElementById("tps").innerHTML = Math.round(1000 / 8.333333333333334)
+		drawTiles(scrollX, scrollY)
+		drawSprites(scrollX, scrollY)
+
+		$("#fps")[0].innerHTML = Math.round(1000 / (performance.now() - fpsC)) //fps counter
+		fpsC = performance.now()
+
+		requestAnimationFrame(draw)
+	}
 }
 
 function runSprites() {
 	for (let sN in cSprites) { //sN is spriteNumber
 		let i = cSprites[sN].update(sN)
 	}
-}
-
-var lastMs = 0
-function draw(ms) {
-	ctx.fillStyle = "magenta"
-	ctx.fillRect(0, 0, 640, 480)//temporary sky
-
-	drawTiles(scrollX, scrollY)
-	drawSprites(scrollX, scrollY)
-
-	document.getElementById("fps").innerHTML = Math.round(1000 / (ms - lastMs))//fps counter
-	lastMs = ms
-
-	requestAnimationFrame(draw)
 }
 
 function drawSprites(ox, oy) {
